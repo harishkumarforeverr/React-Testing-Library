15651c64a3e788a72acb5f052b3f423e5359b5c2
harishkumarforever <harishking809@gmail.com>
2024-03-02 20:23:03 +0530

    wdwe

--- src/Home.test.js
+++ src/Home.test.js
@@ -10,11 +10,8 @@ test("renders welcome message", () => {
 test("displays correct message after button click", () => {
   render(<Home />);
   const button = screen.getByText("Click me!");
-
-  // Click the button once
   fireEvent.click(button);
 
-  // Assert that the message changes
   const clickedOnceMessage = screen.getByText("Button clicked once!");
   expect(clickedOnceMessage).toBeInTheDocument();
 });
@@ -23,25 +20,19 @@ test("displays correct message after button click", () => {
   render(<Home />);
   const button = screen.getByText("Click me!");
 
-  // Click the button multiple times
   fireEvent.click(button);
   fireEvent.click(button);
   fireEvent.click(button);
-
-  // Assert that the message changes
-  const clickedMultipleTimesMessage = screen.getByText("Button clicked 3 times!");
+  const clickedMultipleTimesMessage = screen.getByText(
+    "Button clicked 3 times!"
+  );
   expect(clickedMultipleTimesMessage).toBeInTheDocument();
 });
-
 
 test("displays fetched data after button click", async () => {
   render(<Home />);
   const button = screen.getByLabelText("fetchData");
-  fireEvent.click(button); 
-  
-  // Assert that loading state is not displayed
+  fireEvent.click(button);
   expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
-
-  // Assert that "okay" message is displayed
   expect(screen.getByText("okay")).toBeInTheDocument();
 });
